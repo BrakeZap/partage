3d36b9499433aa1da4c8ac6f0b0c04f2cd73bb41
BrakeZap <kyle.j.smith56@gmail.com>
2025-02-18 20:13:13 -0800

    Scaffolding for the join and create commands

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
+use clap::Args;
 use clap::Parser;
+use clap::Subcommand;
 
-#[derive(Parser, Debug)]
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a new lobby with a random code
+    Create,
+
+    /// Join a lobby with a specified code
+    Join(JoinArgs),
+}
+
+#[derive(Args)]
+struct JoinArgs {
+    /// A valid id to join
+    #[arg(value_parser = valid_id)]
+    join: String,
+}
+
+#[derive(Parser)]
 #[command(version, about="A simple cli chat client built in Rust!", long_about=None)]
-struct Args {
-    #[arg(default_value = "Bob")]
-    name: String,
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+const ID_RANGE: std::ops::RangeInclusive<char> = 'a'..='Z';
+fn valid_id(id: &str) -> Result<String, String> {
+    match id.chars().all(|x| ID_RANGE.contains(&x)) {
+        true => Ok(id.to_string()),
+        false => Err("Invalid id.".to_string()),
+    }
 }
 
 fn main() {
-    let args = Args::parse();
-    println!("Hi {}!", args.name)
+    let args = Cli::parse();
 }
