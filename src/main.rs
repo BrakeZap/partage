7e10c747530546f3d9d4e2f2e94b0163788102c8
BrakeZap <kyle.j.smith56@gmail.com>
2025-02-18 22:00:57 -0800

    Added id generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Args;
 use clap::Parser;
 use clap::Subcommand;
-
+use rand::Rng;
 #[derive(Subcommand)]
 enum Commands {
     /// Create a new lobby with a random code
@@ -24,14 +24,43 @@ struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
-const ID_RANGE: std::ops::RangeInclusive<char> = 'a'..='Z';
+const LOWER_CHAR_RANGE: std::ops::RangeInclusive<char> = 'a'..='z';
+const UPPER_CHAR_RANGE: std::ops::RangeInclusive<char> = 'A'..='Z';
+const NUM_RANGE: std::ops::RangeInclusive<u32> = 0..=9;
 fn valid_id(id: &str) -> Result<String, String> {
-    match id.chars().all(|x| ID_RANGE.contains(&x)) {
-        true => Ok(id.to_string()),
-        false => Err("Invalid id.".to_string()),
+    match id.chars().all(|x| x.is_alphanumeric()) {
+        true => {
+            match id.chars().all(|x| {
+                id.len() == 6
+                    && (LOWER_CHAR_RANGE.contains(&x)
+                        || UPPER_CHAR_RANGE.contains(&x)
+                        || NUM_RANGE.contains(&x.to_digit(10).unwrap()))
+            }) {
+                true => Ok(id.to_string()),
+                false => Err("Id not in range or invalid length".to_string()),
+            }
+        }
+        false => Err("Invalid characters in id.".to_string()),
     }
 }
 
+fn generate_id() -> String {
+    let mut new_string = String::with_capacity(6);
+    let mut rng = rand::rng();
+    for _ in 0..6 {
+        new_string.push(rng.sample(rand::distr::Alphanumeric) as char);
+    }
+    new_string
+}
+
 fn main() {
-    let args = Cli::parse();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Create => println!(
+            "You have successfully created a new room with the id: {}",
+            generate_id()
+        ),
+        Commands::Join(join_args) => todo!(),
+    }
 }
